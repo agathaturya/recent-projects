9b23e52fff87addb46ab1b7e2ad7adda90638a94
agathaturya <agathaturyahikayo14@gmail.com>
2016-12-21 10:05:32 -0500

    Create  SquareRootProgression.java

---
+++ SquareRootProgression.java
@@ -0,0 +1,27 @@
+/*
+ * Agatha Turyahikayo
+ * 11/21/16
+ * SquareRootProgression.java
+ *  each value is the square root of the previous value, represented as a
+ *Double
+ *includes a default constructor that has 65,536 as the first value and a
+ *parametric constructor that starts with a specified number as the first value.
+ */
+public class SquareRootProgression extends AbstractProgression<Double> {
+
+	// default constructor
+	public SquareRootProgression() {
+		current = 65536.0;
+	}// end constructor
+
+	public SquareRootProgression(Double start) {
+		current = start;
+	}// end constructor
+
+	// preconditions:none
+	// postconditions:returns the next value
+	protected void advance() {
+		current = Math.sqrt((Double) current);
+	}// end advance
+
+}// end SquareRootProgression
